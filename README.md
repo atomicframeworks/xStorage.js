1070115af4b816e1e6ea582d477a77bb8fdfba04
Kevin McGinty <atomic.frameworks@gmail.com>
2014-02-19 13:35:19 -0600

    Initial Commit
    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# xStorage.js
+Cross domain localStorage asynchronously<br>
+
+## About
+
+
+## Installation
+1. Include the xStorage.js file on the page where you wish to have cross domain localStorage. No other requirements to start using xStorage.js! If you do not feel like hosting your own storage proxy simply use the standard settings. <br>
+```html
+<script src="xStorage.js"></script>
+```
+This script will create the xStorage object which has methods to access the cross domain localStorage.
+
+## Usage
+
+## License 
+xStorage.js is released under the MIT license <br>
+[www.opensource.org/licenses/MIT](www.opensource.org/licenses/MIT)
---
+++ xStorage.js
@@ -0,0 +1,187 @@
+var xStorage = (function(globals) {
+    // Object to store deferred callbacks
+    var deferredObject = {},
+        // The variable on proxy localStorage that wil used as an object store
+        storageKey = 'xStorage',
+        // The base domain of the proxy
+        proxyDomain = 'http://atomicframeworks.github.io',
+        // The page where the proxy JavaScript is stored
+        proxyPage = '/xStorage.js',
+        // Create proxy iframe
+        iframe = document.createElement("iframe"),
+        // Window object used to pass messages between the iframes
+        proxyWindowObject,
+        Deferred = (globals.Promise) ? globals.Promise : function(func) {
+            var that = this;
+            // Fake promise interface if Promise not available
+            that.callbacks = [];
+            that.errorbacks = [];
+            that.then = function(callback, errorback) {
+                if (errorback) {
+                    that.errorbacks[that.callbacks.length] = errorback;
+                }
+                that.callbacks.push(callback);
+                return that;
+            };
+            that.resolve = function(data) {
+                for (var i = 0; i < that.callbacks.length; i++) {
+                    try {
+                        data = that.callbacks[i](data);
+                    } catch (e) {
+                        if (that.errorbacks[i]) {
+                            that.errorbacks[i](e);
+                        } else {
+                            throw new Error(e);
+                        }
+                    }
+                }
+            };
+            that.reject = function(e) {
+                if (that.errorbacks.length) {
+                    that.errorbacks[that.errorbacks.length](e);
+                } else {
+                    throw new Error(e);
+                }
+            };
+            if (func) {
+                func(that.resolve, that.reject);
+            }
+        };
+
+    proxyDomain = 'http://localhost:8000';
+    proxyPage = '/proxy.html';
+    // Add the proxy iframe to DOM
+    iframe.setAttribute("src", proxyDomain + proxyPage);
+    iframe = document.body.appendChild(iframe);
+    proxyWindowObject = iframe.contentWindow;
+   
+    // Helper function to determine if empty object
+    function isEmptyObject(item) {
+        for(var prop in item) {
+            if (item.hasOwnProperty(prop)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    // Helper function to determine if the item is an object (not including arrays)
+    function isObject(item) {
+        return (item instanceof Object && typeof(item) === 'object' && ! (item instanceof Array) );
+    }
+
+    // Helper to return a random string to serve as a simple hash
+    function randomHash() {
+        return Math.random().toString(36).substr(2);
+    }
+
+    // Function to get localStorage from proxy
+    function getStorage(property) {
+        return new Deferred(function(resolve, reject) {
+            // Message to get storage
+            var hash = randomHash(),
+                message = JSON.stringify({
+                    'event': 'get',
+                    'storageKey': storageKey,
+                    'item': property,
+                    'deferredHash': hash
+                });
+            // Set the deferred object reference
+            deferredObject[hash] = {
+                resolve: resolve,
+                reject: reject,
+            };
+            // Send the message and target URI
+            proxyWindowObject.postMessage(message, '*'); 
+        });
+    }
+
+    // Helper to create the functions for promises
+    function createPromiseFunction(event, item) {
+        return function(resolve, reject) {
+            // Message to set the storage
+            var hash = randomHash(),
+                message = JSON.stringify({
+                    'event': event,
+                    'storageKey': storageKey,
+                    'deferredHash': hash,
+                    'item': item
+                });
+            // Set the deferred object reference
+            deferredObject[hash] = {
+                resolve: resolve,
+                reject: reject,
+            };
+            // Send the message and target URI
+            proxyWindowObject.postMessage(message, '*');
+        };
+    }
+
+    // Function to set localStorage on proxy
+    function setStorage(property, value) {
+        if (typeof property !== 'string' && typeof property !== 'number') {
+            throw new Error('Property argument must be a string or number to set a specific property');
+        }
+        return new Deferred(
+            createPromiseFunction('set', {
+                'property': property,
+                'value': value
+            })
+        );
+    }
+
+    // Function to merge xStorage on proxy with storageObject
+    function mergeStorage(storageObject) {
+        if (!isObject(storageObject)) {
+            throw new Error('Argument must be an object to merge with xStorage object');
+        }
+        return new Deferred(
+            createPromiseFunction('merge', storageObject)
+        );
+    }
+
+    // Delete a property from storage or entire storage if no property provided
+    function deleteStorage(property) {
+        return new Deferred(
+            createPromiseFunction('delete', {
+                'property': property,
+                'value': null
+            })
+        );      
+    }
+    
+    // Bind the message event for getting storage
+    (function bindWindow () {
+        // Bind the message back listener
+        window.addEventListener('message', function(event) {
+            if( event.origin == proxyDomain) {
+                // Parse the response
+                response = JSON.parse(event.data);
+                // if there is a deferred object resolve and remove
+                if (response.deferredHash) {
+                    if (response.error) {
+                        // Reject if error                        
+                        deferredObject[response.deferredHash].reject(response.error);
+                    } else {
+                        // Resolve the deferred object
+                        deferredObject[response.deferredHash].resolve(response.storageObject);
+                    }
+                    // Remove the deferred item
+                    delete deferredObject[response.deferredIndex];
+                } else if (response.error) {
+                    throw new Error(response.error);
+                }
+
+            } else {
+                console.log('bad domain: ' + proxyDomain +  '  origin:' + event.origin);
+            }
+        }, false);
+    }());
+    // Return the public methods
+    return {
+        get: getStorage,
+        set: setStorage,
+        merge: mergeStorage,
+        delete: deleteStorage
+    };
+}(this));
